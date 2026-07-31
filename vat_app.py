8e666dda90c55935042ef3e0df62c48537d6124e
407-36-nutchaphat <56413@yupparaj.ac.th>
2026-07-31 08:26:50 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -4,7 +4,7 @@ st.title(". แอปพลิเคชั่นคำนวณราคาส�
 price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
 vat = price * 0.07
 net_price = price - vat
-st.header(f' ภาษีมูลค่าเพิ่ม (VAT 7%): **(vat: 21)** บาท")
+st.header(f' ภาษีมูลค่าเพิ่ม (VAT 7%): **(vat:.2f)** บาท")
 st.header(f"• ราคาสุทธิ : (net_price: 21) บาท")
 st.divider()
 st.write("นางสาวณัชภัส สง่าเมือง เลขที่ 36 ม.4/7")
