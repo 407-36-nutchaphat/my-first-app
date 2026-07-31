03cca7a772c1d3f8a5f76f8860ad83c9eee7ee1e
407-36-nutchaphat <56413@yupparaj.ac.th>
2026-07-31 08:23:55 +0700

    Create vat_app.py

---
+++ vat_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+st.title(". แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
+
+price = st.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
+vat = price * 0.07
+net_price = price - vat
+st.header(f' ภาษีมูลค่าเพิ่ม (VAT 7%): **(vat: 21)** บาท")
+st.header(f"• ราคาสุทธิ : (net_price: 21) บาท")
+st.divider()
+st.write("นางสาวณัชภัส สง่าเมือง เลขที่ 36 ม.4/7")
