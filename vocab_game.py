50056f3982ec5939b91fadde91a39519e2117d5e
407-36-nutchaphat <56413@yupparaj.ac.th>
2026-09-11 09:15:39 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -22,7 +22,7 @@ def reset_game():
 # 📌 ฟังก์ชัน MessageBox (Dialog)
 # ----------------------------------------------------
 @st.dialog("📊 สรุปผลการเล่นเกม")
-def show_result_dialog(ans1, ans2):
+def show_result_dialog(ans1, ans2, ans4, ans5):
     st.balloons()
     score = 0
 
