bd366473bd5a93d353f100755eb7f6ca3368955a
407-36-nutchaphat <56413@yupparaj.ac.th>
2026-09-11 08:33:27 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -96,7 +96,7 @@ ans3 = st.text_input(
     value=st.session_state.ans3_val,
 )
 ans4 = st.text_input(
-    "ข้อ 4: I have a really `c _ t _ `dog. 🐶",(
+    "ข้อ 4: I have a really `c _ t _ `dog. 🐶",
     value=st.session_state.ans4_val,
 )
 # อัปเดตค่าล่าสุดเข้าตัวแปร
